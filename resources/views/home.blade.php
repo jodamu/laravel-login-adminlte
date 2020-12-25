21adb4ad17ba76ec1ddf4e0320275acc2e3b7026
jodamu <jodamu90@gmail.com>
2020-12-25 15:25:57 -0500

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,55 +17,7 @@
         </div>
     </div>
 
-    <div class="card">
-        <div class="card-header">
-            <h1 class="card-title">Hola mundo</h1>
-        </div>
-
-        <div class="card-body">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="card-header">
-            <h1 class="card-title">Hola mundo</h1>
-        </div>
-
-        <div class="card-body">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="card-header">
-            <h1 class="card-title">Hola mundo</h1>
-        </div>
-
-        <div class="card-body">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="card-header">
-            <h1 class="card-title">Hola mundo</h1>
-        </div>
-
-        <div class="card-body">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
-        </div>
-    </div>
-
-    <div class="card">
-        <div class="card-header">
-            <h1 class="card-title">Hola mundo</h1>
-        </div>
-
-        <div class="card-body">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, et obcaecati autem magnam explicabo repellat distinctio, similique accusamus, maiores in praesentium voluptate atque non magni at iste perferendis sint fugiat?</p>
-        </div>
-    </div>
+   
 @stop
 
 @section('css')
